9f2c4392985035b927782781de445a27ddbcedef
BimalKanxa <sharmabimal2003@gmail.com>
2023-07-12 06:13:20 +0530

    adding codes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert.js'
+
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      showAlert(null)
+    }, 2000)
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = "#341a1a"
+      showAlert("Dark Mode has been enabled", "success")
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = "white"
+      showAlert("Light Mode has been enabled", "success")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Router> */}
+      <Navbar title="Text Utils Web" aboutText="About Us" mode={mode} toggleMode={toggleMode}></Navbar>
+      <Alert alert={alert}>  </Alert>
+      
+      <div className="container my-5">
+          {/* <Routes> */}
+          {/* <Route exact path="/" element={<Textform heading="Enter the text to analyze below :" mode={mode} />} /> */}
+
+{/*     
+          <Route exact path='/about' element={<About/>}>
+            </Route>    */}
+
+    <Textform heading="Enter the text to analyze below : " mode={mode}></Textform>
+
+        
+{/* </Routes> */}
+      </div>
+      {/* </Router> */}
+
+    </>
   );
 }
 
---
+++ src/components/Textform.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+
+export default function Textform(props) {
+
+    const [text, setText] = useState("");
+    const [isbold, setisBold] = useState(false)
+
+    const handleUpClick = () => {
+        let newtext = text.toUpperCase();
+        setText(newtext)
+        // console.log("uppercase was clicked")
+    }
+    const handleOnChange = (event) => {
+        // console.log("OnChanged Function Triggered")
+        setText(event.target.value)
+    }
+
+    const handleOnFocus = () => {
+        if (text === "enter text here") {
+            setText("");
+        }
+    };
+    const handleDownClick = () => {
+        const newtext = text.toLowerCase()
+        setText(newtext)
+    }
+
+    const handleClearClick = () =>{
+        setText("")
+    }
+    const handleBold = () =>{
+        setisBold(!isbold)
+    }
+    const countWord = (text) =>{
+        const words = text.trim().split(/\s+/);
+        const filterWords = words.filter(word => word !== '')
+        return filterWords.length
+    }
+
+    return (
+        <>
+            <div className='container'  style={{color:props.mode==='dark'?'white':'black'}}>
+                <h1>
+                    {props.heading}
+                </h1>
+                <div className="mb-3">
+                    {/* <label for="myBox" className="form-label">Example textarea</label> */}
+                    <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="10" onFocus={handleOnFocus} style={{backgroundColor:props.mode==='dark'?'#341a1a':'white',color:props.mode==='dark'?'white':'black', fontWeight: isbold ? 'bold' : 'normal' }}></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className='btn btn-secondary mx-2' onClick={handleDownClick}>Convert to Lowercase</button>
+                <button className='btn btn-dark mx-2' onClick={handleBold}>Convert to Bold</button>
+                <button className='btn btn-danger mx-2' onClick={handleClearClick}>Clear</button>
+            </div>
+
+            <div className="container my-3"  style={{color:props.mode==='dark'?'white':'black'}}>
+                <h2>Your text summary</h2>
+                <p>{countWord(text)} words and {text.length} characters</p>
+                <p>{0.008 * countWord(text)} Minutes read</p>
+
+                <h3>Preview</h3>
+                <p>{text.length>0?text:"enter text above to preview here"}</p>
+            </div>
+
+        </>
+    )
+}
